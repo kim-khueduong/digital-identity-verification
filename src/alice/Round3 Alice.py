afc00b6f235250558deb04a0786a09344151d401
Debbi-eeeee <debbieogiesoba20@gmail.com>
2026-09-11 20:20:49 +1000

    Update Round3 Alice.py

--- src/alice/Round3 Alice.py
+++ src/alice/Round3 Alice.py
@@ -1,1 +1,24 @@
+import hmac
+impport haslib
+
+print ("ALICE: LEGITIMATE SENDER")
+message = input("Enter a message to send to Bob:")
+
+#Alice and Bob uses the same key
+SHARED_KEY = b"shared-secret-key"
+
+#Generate HMAC-SHA256 of the message using the shared key
+message_hmac = hmac.new(SHARED_KEY, message.encode(), hashlib.sha256).hexdigest()
+
+#save the message 
+with open("src/shared/message.txt", "w") as file:
+   file.write(message_hmac)
+
+print()
+print("Alice's sent:")
+print(message)
+
+print()
+print("HAMC-SHA256 (using shared key):")
+print(message_hmac)
 
